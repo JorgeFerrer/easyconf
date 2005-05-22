268cf20ae5fe3e4b6084823f10a6f6b92216df5a
Álvaro González de Paz <kortatu@users.noreply.github.com>
2005-05-22 22:01:40 +0000

    Added JavaDocs comment. Added Constructor taking a ComponentConfiguration as parameter. Improved style.

--- easyconf/src/java/com/germinus/easyconf/jmx/ComponentConfigurationDynamicMBean.java
+++ easyconf/src/java/com/germinus/easyconf/jmx/ComponentConfigurationDynamicMBean.java
@@ -42,269 +42,364 @@ import com.germinus.easyconf.ComponentProperties;
 import com.germinus.easyconf.EasyConf;
 
 /**
- * <a href="ComponentConfigurationDynamicMBean.java.html"><b><i>View Source</i></b></a>
+ * MBean wrapper for a ComponentConfiguration. <a
+ * href="ComponentConfigurationDynamicMBean.java.html"> <b><i>View Source </i>
+ * </b> </a>
  *
- * @author  Alvaro González
+ * @author Alvaro González
  * @version $Revision$
  *
  */
 public class ComponentConfigurationDynamicMBean implements DynamicMBean {
-	public static final String RELOAD_OPERATION_NAME = "reloadConfiguration";
-	public static final String NEW_PROPERTY_OPERATION_NAME = "newProperty";
-	public static final String[] RELOAD_OPERATION_SIGNATURE = new String[0];	
-	public static final String[] NEW_PROPERTY_OPERATION_SIGNATURE_1 = new String[]{String.class.toString()};
-	public static final String[] NEW_PROPERTY_OPERATION_SIGNATURE_2 = new String[]{String.class.toString(),Object.class.toString()};
-	private static final String NEW_PROPERTY_OPERATION_DESCRIPTION_1 =
-		"Add new property to the Configuration Component. The Param is the property name. Must not"+
+    /**
+     * Name of the reloadConfiguration operation. Used when invoking
+     * <code>invoke</code> method.
+     */
+    public static final String RELOAD_OPERATION_NAME = "reloadConfiguration";
+
+    /**
+     * Name of the newProperty operations. Used when invoking
+     * <code>invoke</code> method.
+     */
+    public static final String NEW_PROPERTY_OPERATION_NAME = "newProperty";
+
+    /**
+     * Signature of the reloadConfiguration operation. Used when invoking
+     * <code>invoke</code> method.
+     */
+    public static final String[] RELOAD_OPERATION_SIGNATURE = new String[0];
+
+    /**
+     * Signature of the newProperty operation with one parameter.
+     * Used when invoking <code>invoke</code> method.
+     */
+    public static final String[] NEW_PROPERTY_OPERATION_SIGNATURE_1 = 
+    	new String[] { String.class.toString() };
+
+    /**
+     * Signature of the newProperty operation with two parameters.
+     * Used when invoking <code>invoke</code> method.
+     */
+    public static final String[] NEW_PROPERTY_OPERATION_SIGNATURE_2 = 
+    	new String[] {String.class.toString(), Object.class.toString() };
+
+    private static final String NEW_PROPERTY_OPERATION_DESCRIPTION_1 = 
+    	"Add new property to the Configuration Component. " +
+    	"The Param is the property name. Must not" +
 		" exists another property with the same name.";
-	private static final String NEW_PROPERTY_OPERATION_DESCRIPTION_2 = NEW_PROPERTY_OPERATION_DESCRIPTION_1+ 
+
+	private static final String NEW_PROPERTY_OPERATION_DESCRIPTION_2 = 
+		NEW_PROPERTY_OPERATION_DESCRIPTION_1 +
 		" The second param is the initial value of the new property";
+
 	private static final String RELOAD_OPERATION_DESCRIPTION = 
 		"Reloads the configuration";
+
 	private static final String CONSTRUCTOR_DESCRIPTION_1 = 
 		"Constructs new MBean with the configuration of the named Component";
+
 	private String componentName;
+
 	private ComponentConfiguration componentConfiguration;
-	private MBeanAttributeInfo[] attributesInfo;	
+
+	private MBeanAttributeInfo[] attributesInfo;
+
 	private Map modifiedProperties;
+
 	private Map newPropeties;
+
 	private MBeanOperationInfo[] operationInfo;
+
 	private MBeanConstructorInfo[] constructorsInfo;
 
-	
-	
-	
-	public ComponentConfigurationDynamicMBean(String componentName){
+	/**
+	 * Create a MBean wraping some loaded ComponentConfiguration 
+	 * @param component
+	 */
+	public ComponentConfigurationDynamicMBean
+		(ComponentConfiguration component) {
 		super();
-		this.componentName=componentName;
-		this.modifiedProperties=new HashMap();
-		this.newPropeties=new HashMap();
-		loadComponentConfiguration();
+		this.componentName=component.getComponentName();
+		this.componentConfiguration=component;
+		init();
 	}
 
 	/**
-	 * 
+	 * Loads and wraps a ComponentConfiguration
+	 * @param componentName
 	 */
-	private void loadComponentConfiguration() {
-		this.componentConfiguration=EasyConf.getConfiguration(componentName);		
+	public ComponentConfigurationDynamicMBean(String componentName) {
+		super();
+		this.componentName = componentName;
+		this.componentConfiguration = EasyConf.getConfiguration(componentName);
+		init();		
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Some initializations common to both constructors
+	 */
+	private void init(){
+		this.modifiedProperties = new HashMap();
+		this.newPropeties = new HashMap();
+	}
+
+	/**
+	 * Obtains the component attributes from the ComponentCongiguration
+	 * 
 	 * @see javax.management.DynamicMBean#getAttribute(java.lang.String)
 	 */
-	public Object getAttribute(String attributeName) throws AttributeNotFoundException,
-			MBeanException, ReflectionException {
-		ComponentProperties prop=componentConfiguration.getProperties();
+	public Object getAttribute(String attributeName)
+			throws AttributeNotFoundException, MBeanException,
+			ReflectionException {
+		ComponentProperties prop = componentConfiguration.getProperties();
 		if (modifiedProperties.containsKey(attributeName))
 			return modifiedProperties.get(attributeName);
 		if (prop.containsKey(attributeName))
 			return prop.getProperty(attributeName);
-		else		
-			if (newPropeties.containsKey(attributeName))
-				return newPropeties.get(attributeName);
-			else
-				throw new AttributeNotFoundException(attributeName);
+		else if (newPropeties.containsKey(attributeName))
+			return newPropeties.get(attributeName);
+		else
+			throw new AttributeNotFoundException(attributeName);
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Sets an attribute
+	 * 
 	 * @see javax.management.DynamicMBean#setAttribute(javax.management.Attribute)
 	 */
-	public void setAttribute(Attribute attribute) throws AttributeNotFoundException,
-			InvalidAttributeValueException, MBeanException, ReflectionException {
-		if (componentConfiguration.getProperties().containsKey(attribute.getName()))
-				modifiedProperties.put(attribute.getName(),attribute.getValue());
+	public void setAttribute(Attribute attribute)
+			throws AttributeNotFoundException, InvalidAttributeValueException,
+			MBeanException, ReflectionException {
+		if (componentConfiguration.getProperties().containsKey(
+				attribute.getName()))
+			modifiedProperties.put(attribute.getName(), attribute.getValue());
+		else if (newPropeties.containsKey(attribute.getName()))
+			newPropeties.put(attribute.getName(), attribute.getValue());
 		else
-			if (newPropeties.containsKey(attribute.getName()))
-				newPropeties.put(attribute.getName(),attribute.getValue());
-			else
-				throw new AttributeNotFoundException(attribute.getName());
+			throw new AttributeNotFoundException(attribute.getName());
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * List named attributes
+	 * 
 	 * @see javax.management.DynamicMBean#getAttributes(java.lang.String[])
 	 */
 	public AttributeList getAttributes(String[] attributesNames) {
-		Set includedAttributeNames=new HashSet();
-		AttributeList attributeList=new AttributeList();
-		for (int i=0;i<attributesNames.length;i++){
-			String attributeName=attributesNames[i];
-			if (!includedAttributeNames.contains(attributeName)){
+		Set includedAttributeNames = new HashSet();
+		AttributeList attributeList = new AttributeList();
+		for (int i = 0; i < attributesNames.length; i++) {
+			String attributeName = attributesNames[i];
+			if (!includedAttributeNames.contains(attributeName)) {
 				includedAttributeNames.add(attributeName);
 				try {
-					Object value=getAttribute(attributeName);
-					Attribute attribute=new Attribute(attributeName,value);
+					Object value = getAttribute(attributeName);
+					Attribute attribute = new Attribute(attributeName, value);
 					attributeList.add(attribute);
 				} catch (AttributeNotFoundException e) {
-					//Ignoring attribute
-				} catch (MBeanException e) {				
-				} catch (ReflectionException e) {				
+					// Ignoring attribute
+				} catch (MBeanException e) {
+				} catch (ReflectionException e) {
 				}
 			}
 		}
 		return attributeList;
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Set named attributes
+	 * 
 	 * @see javax.management.DynamicMBean#setAttributes(javax.management.AttributeList)
 	 */
 	public AttributeList setAttributes(AttributeList attributes) {
-		AttributeList modifieds=new AttributeList();
-		Iterator it=attributes.iterator();
+		AttributeList modifieds = new AttributeList();
+		Iterator it = attributes.iterator();
 		while (it.hasNext()) {
 			Attribute attribute = (Attribute) it.next();
 			try {
 				setAttribute(attribute);
 				modifieds.add(attribute);
 			} catch (AttributeNotFoundException e) {
-			} catch (InvalidAttributeValueException e) {				
-			} catch (MBeanException e) {				
-			} catch (ReflectionException e) {			
+			} catch (InvalidAttributeValueException e) {
+			} catch (MBeanException e) {
+			} catch (ReflectionException e) {
 			}
 		}
 		return modifieds;
 	}
 
-	/* (non-Javadoc)
-	 * @see javax.management.DynamicMBean#invoke(java.lang.String, java.lang.Object[], java.lang.String[])
+	/**
+	 * Invoke one of the operations exposed by the MBeanas. 
+	 * This Operations could be one of:
+	 * <ul>
+	 *   <li><code>void newProperty(String propertyName)</code></li>
+	 *   <li>
+	 *   	<code>void newProperty(String propertyName, String initialValue)
+	 *   	</code>
+	 *   </li>
+	 *   <li><code>void reloadConfiguration()</code></li>
+	 * </ul>
+	 * 
+	 * @see javax.management.DynamicMBean#invoke(java.lang.String,
+	 *      java.lang.Object[], java.lang.String[])
 	 */
-	public Object invoke(String operationName, Object[] params, String[] signature)
-			throws MBeanException, ReflectionException {
-		if (operationName.equals(RELOAD_OPERATION_NAME)){
+	public Object invoke(String operationName, Object[] params,
+			String[] signature) throws MBeanException, ReflectionException {
+		if (operationName.equals(RELOAD_OPERATION_NAME)) {
 			reloadConfiguration();
 			return null;
 		}
-		if (operationName.equals(NEW_PROPERTY_OPERATION_NAME)){
-			if (signature.length==1)
-				newProperty((String)params[0]);
-			else if (signature.length==2)
-				newProperty((String)params[0],params[1]);
+		if (operationName.equals(NEW_PROPERTY_OPERATION_NAME)) {
+			if (signature.length == 1)
+				newProperty((String) params[0]);
+			else if (signature.length == 2)
+				newProperty((String) params[0], params[1]);
 			else
-				throw new MBeanException(new IllegalArgumentException("Operation not found in MBEan: "+operationName+"("+signature+")"));
+				throw new MBeanException(new IllegalArgumentException(
+						"Operation not found in MBEan: " + operationName + "("
+								+ signature + ")"));
 			return null;
 		}
-		throw new MBeanException(new IllegalArgumentException("Operation not found in MBEan: "+operationName+"("+signature+")"));
-	}
-	
-	//***** Implementations of the MBean's operations *****//
-	
-	private void reloadConfiguration(){
-		this.componentConfiguration=EasyConf.getConfiguration(componentName);
-	}
-	
-	private void newProperty(String propertyName) throws MBeanException{
-		newProperty(propertyName,null);
-	}
-	
-	private void newProperty(String propertyName,Object value) throws MBeanException{
-		ComponentProperties prop=componentConfiguration.getProperties();
-		if ((newPropeties.containsKey(propertyName)) || prop.containsKey(propertyName))
-			throw new MBeanException(new IllegalArgumentException("Cannot add new propert whith name: "+propertyName+". There is a property whith that name yet"));
-		newPropeties.put(propertyName,value);
+		throw new MBeanException(new IllegalArgumentException(
+				"Operation not found in MBEan: " + operationName + "("
+						+ signature + ")"));
 	}
 
-	/* (non-Javadoc)
+	// ***** Implementations of the MBean's operations *****//
+
+	private void reloadConfiguration() {
+		this.componentConfiguration = EasyConf.getConfiguration(componentName);
+	}
+
+	private void newProperty(String propertyName) throws MBeanException {
+		newProperty(propertyName, null);
+	}
+
+	private void newProperty(String propertyName, Object value)
+			throws MBeanException {
+		ComponentProperties prop = componentConfiguration.getProperties();
+		if ((newPropeties.containsKey(propertyName))
+				|| prop.containsKey(propertyName))
+			throw new MBeanException(new IllegalArgumentException(
+					"Cannot add new propert whith name: " + propertyName
+							+ ". There is a property whith that name yet"));
+		newPropeties.put(propertyName, value);
+	}
+
+	/**
+	 * Return the Information exposed by the MBean: Attributes, Operations,
+	 * Constructors and Notifications.
+	 * 
 	 * @see javax.management.DynamicMBean#getMBeanInfo()
 	 */
 	public MBeanInfo getMBeanInfo() {
-		return new MBeanInfo(
-				this.getClass().toString(),
-				"Easyconf component: "+this.componentName,
-				getAttributeInfo(),
-				getConsturctorsInfo(),
-				getOperationInfo(),
-				null);
+		return new MBeanInfo(this.getClass().toString(), "Easyconf component: "
+				+ this.componentName, getAttributeInfo(),
+				getConsturctorsInfo(), getOperationInfo(), null);
 	}
-	
-	protected MBeanAttributeInfo[] getAttributeInfo(){
-		if (attributesInfo==null){
-			ComponentProperties properties=componentConfiguration.getProperties();
-			List auxList=new ArrayList();			
-			Iterator it=properties.getKeys();			
-			while (it.hasNext()) {				
+
+	/**
+	 * Constructs the info of the MBean's attributes.
+	 * 
+	 * @return Array of MBeanAttributeInfo
+	 */
+	protected MBeanAttributeInfo[] getAttributeInfo() {
+		if (attributesInfo == null) {
+			ComponentProperties properties = componentConfiguration
+					.getProperties();
+			List auxList = new ArrayList();
+			Iterator it = properties.getKeys();
+			while (it.hasNext()) {
 				String propertyName = (String) it.next();
-				Object value=properties.getProperty(propertyName);
-				String type=value.getClass().getName();				
-				boolean isIs=value.getClass().equals(Boolean.class);
-				MBeanAttributeInfo attributeInfo=
-					new MBeanAttributeInfo(propertyName,
-							type,
-							"Easyconf "+componentName+" component property: "+propertyName,
-							true,
-							true,
-							isIs);
+				Object value = properties.getProperty(propertyName);
+				String type = value.getClass().getName();
+				boolean isIs = value.getClass().equals(Boolean.class);
+				MBeanAttributeInfo attributeInfo = new MBeanAttributeInfo(
+						propertyName, type, "Easyconf " + componentName
+								+ " component property: " + propertyName, true,
+						true, isIs);
 				auxList.add(attributeInfo);
-				this.attributesInfo=(MBeanAttributeInfo[]) auxList.toArray(new MBeanAttributeInfo[auxList.size()]);				
+				this.attributesInfo = (MBeanAttributeInfo[]) auxList
+						.toArray(new MBeanAttributeInfo[auxList.size()]);
 			}
 		}
 		return this.attributesInfo;
 	}
-	
-	protected MBeanOperationInfo[] getOperationInfo(){
-		if (operationInfo==null){
-			operationInfo = new MBeanOperationInfo[]{
-				new MBeanOperationInfo(NEW_PROPERTY_OPERATION_NAME,
-					NEW_PROPERTY_OPERATION_DESCRIPTION_1,
-					//Parameters
-					new MBeanParameterInfo[]{
-						new MBeanParameterInfo("propertyName",String.class.getName(),"Name of the new property")						
-					},
-					void.class.getName(),
-					MBeanOperationInfo.ACTION
-				),
-				new MBeanOperationInfo(NEW_PROPERTY_OPERATION_NAME,
-					NEW_PROPERTY_OPERATION_DESCRIPTION_2,
-					//Parameters
-					new MBeanParameterInfo[]{
-						new MBeanParameterInfo("propertyName",String.class.getName(),"Name of the new property"),
-						new MBeanParameterInfo("value",Object.class.getName(),"Initial value of the new property")
-					},
-					void.class.getName(),
-					MBeanOperationInfo.ACTION
-				),
-				new MBeanOperationInfo(RELOAD_OPERATION_NAME,
-					RELOAD_OPERATION_DESCRIPTION,
-					//Parameters
-					new MBeanParameterInfo[]{},
-					void.class.getName(),
-					MBeanOperationInfo.ACTION
-				)								
-			};
-			                                       	
+
+	/**
+	 * Constructs the info of the MBean's operations.
+	 * 
+	 * @return Array of MBeanOperationInfo
+	 */
+	protected MBeanOperationInfo[] getOperationInfo() {
+		if (operationInfo == null) {
+			operationInfo = new MBeanOperationInfo[] {
+					new MBeanOperationInfo(NEW_PROPERTY_OPERATION_NAME,
+							NEW_PROPERTY_OPERATION_DESCRIPTION_1,
+							// Parameters
+							new MBeanParameterInfo[] { new MBeanParameterInfo(
+									"propertyName", String.class.getName(),
+									"Name of the new property") }, void.class
+									.getName(), MBeanOperationInfo.ACTION),
+					new MBeanOperationInfo(
+							NEW_PROPERTY_OPERATION_NAME,
+							NEW_PROPERTY_OPERATION_DESCRIPTION_2,
+							// Parameters
+							new MBeanParameterInfo[] {
+									new MBeanParameterInfo("propertyName",
+											String.class.getName(),
+											"Name of the new property"),
+									new MBeanParameterInfo("value",
+											Object.class.getName(),
+											"Initial value of the new property") },
+							void.class.getName(), MBeanOperationInfo.ACTION),
+					new MBeanOperationInfo(RELOAD_OPERATION_NAME,
+							RELOAD_OPERATION_DESCRIPTION,
+							// Parameters
+							new MBeanParameterInfo[] {}, void.class.getName(),
+							MBeanOperationInfo.ACTION) };
+
 		}
 		return operationInfo;
 	}
-	
-	protected MBeanConstructorInfo[] getConsturctorsInfo(){
-		if (constructorsInfo==null){
-			Constructor constructor=null;
+
+	/**
+	 * Constructs an array of the MBean's constructors.
+	 * 
+	 * @return Array of MBeanConstructorInfo
+	 */
+	protected MBeanConstructorInfo[] getConsturctorsInfo() {
+		if (constructorsInfo == null) {
+			Constructor constructor = null;
 			try {
-				constructor = this.getClass().getConstructor(new Class[]{String.class});
+				constructor = this.getClass().getConstructor(
+						new Class[] { String.class });
 			} catch (Exception e) {
 			}
-			constructorsInfo=new MBeanConstructorInfo[]{
-					new MBeanConstructorInfo(CONSTRUCTOR_DESCRIPTION_1,constructor)
-			};
+			constructorsInfo = new MBeanConstructorInfo[] { new MBeanConstructorInfo(
+					CONSTRUCTOR_DESCRIPTION_1, constructor) };
 		}
 		return constructorsInfo;
 	}
-	
-	
 
 	/**
-	 * @return Returns the componentConfiguration.
+	 * Returns the ComponentConfiguration associated with this MBean.
+	 * 
+	 * @return ComponentConfiguration.
 	 */
 	protected ComponentConfiguration getComponentConfiguration() {
 		return componentConfiguration;
 	}
+
 	/**
-	 * @param componentConfiguration The componentConfiguration to set.
+	 * @param componentConfiguration
+	 *            The componentConfiguration to set.
 	 */
 	protected void setComponentConfiguration(
 			ComponentConfiguration componentConfiguration) {
 		this.componentConfiguration = componentConfiguration;
 	}
-	
-	private boolean propertyExists(String property){
+
+	private boolean propertyExists(String property) {
 		if (componentConfiguration.getProperties().containsKey(property))
 			return true;
 		if (newPropeties.containsKey(property))
